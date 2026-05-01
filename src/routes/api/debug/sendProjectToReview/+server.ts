80eab19aa475c4f08c91c63cc4a85315a485a9d5
Peleg <45825608+pkd2210@users.noreply.github.com>
2026-05-01 19:01:27 +0300

    Add descriptions for debug file

--- src/routes/api/debug/sendProjectToReview/+server.ts
+++ src/routes/api/debug/sendProjectToReview/+server.ts
@@ -2,14 +2,14 @@ import { sendProjectToReview } from "$lib/db/airtableClient";
 
 export async function GET() {
     await sendProjectToReview(
-        "U091DE0M4NB",
-        1,
-        "hackatime project",
-        "https://cdn.hackclub.com/019d8810-944d-7e3e-9650-9650b638ab40/61RahTQtAqL._AC_UF894,1000_QL80_.jpg",
-        "a description",
-        "pkd2210",
-        "https://github.com",
-        "https://google.com"
+        "U091DE0M4NB", // slackId
+        1, // journeyNumber
+        "hackatime project", // HackatimeProjectName
+        "https://cdn.hackclub.com/019d8810-944d-7e3e-9650-9650b638ab40/61RahTQtAqL._AC_UF894,1000_QL80_.jpg", // screenshotUrl
+        "a description", // description
+        "pkd2210", // githubUsername
+        "https://github.com", // codeUrl
+        "https://google.com" // playableUrl
     );
 
     return new Response(JSON.stringify({ ok: true }), {
