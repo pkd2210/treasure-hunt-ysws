5c727c777a089e296fef65a6a6346c185025f443
Peleg <45825608+pkd2210@users.noreply.github.com>
2026-05-02 18:22:39 +0300

    Update footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,10 +11,12 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
 	<link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet" />
 </svelte:head>
-{@render children()}
-<!--A footer-->
-<footer>
-	<p style="text-align: center; padding: 1rem; background-color: #f0f0f0; margin: 0;">
-		Made With ❤️ By teens from <a href="https://hackclub.com" style="text-decoration: underline;">Hack Club</a> A 503(c)(3) nonprofit organization.
-	</p>
-</footer>
+<div class="flex flex-col min-h-screen">
+	{@render children()}
+	<!--A footer-->
+	<footer class="w-full mt-auto">
+		<p style="text-align: center; padding: 1rem; background-color: #f0f0f0; margin: 0;">
+			Made With ❤️ By teens from <a href="https://hackclub.com" style="text-decoration: underline;">Hack Club</a> A 503(c)(3) nonprofit organization.
+		</p>
+	</footer>
+</div>
